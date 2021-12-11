02ece9c952084463a350aef0d2a20f4d83a7a314
Abdullah Mohamed <61066251+Abdullah3010@users.noreply.github.com>
2021-12-11 16:29:42 +0200

    Random initial state

---
+++ creator.py
@@ -0,0 +1,31 @@
+import numpy as np
+import random
+import math
+
+class Creator:
+
+    # N is the size of puzzle ex ({8,9 Puzzle},{15,16 Puzzle},{24,25 Puzzle})
+    def __init__(self,N):
+        self.N = int(math.sqrt(N+1))
+
+
+
+    def initial_state(self):
+
+        initState = np.zeros((self.N,self.N),dtype=int)
+        a = []
+
+        #create random array with distinct values in the range
+        while(len(a) != self.N*self.N):
+            n = random.randint(0,self.N*self.N-1)
+            if not a.__contains__(n):
+                a = a +[n]
+
+        #copy the distinct array in matrix  
+        index = 0
+        for i in range(0,self.N):
+            for j in range(0,self.N):
+                initState[i][j] = a[index]
+                index = index+1
+            
+        return initState
