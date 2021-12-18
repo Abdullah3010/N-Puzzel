3545cfb7abff99cf8bc8679ce3b6f1d162d84b05
Abdullah Mohamed <61066251+Abdullah3010@users.noreply.github.com>
2021-12-18 23:53:57 +0200

    heuristic done

--- Board.py
+++ Board.py
@@ -1,4 +1,6 @@
-import cmath
+import numpy as np
+import math as m
+
 class Board:
     UP = 'up'
     DOWN = 'down'
@@ -27,7 +29,7 @@ class Board:
 
     def incMovCounter(self):
         self.__MovCounter += 1
-
+        
     def resetMovCounter(self):
         self.__MovCounter = 0
 
@@ -35,6 +37,7 @@ class Board:
         # Return a board data structure with tiles in the solved state.
         # For example, if BOARDWIDTH and BOARDHEIGHT are both 3, this function
         # returns [[1, 4, 7], [2, 5, 8], [3, 6, BLANK]]
+        
         self.__MovCounter = 0
         counter = 1
         board = []
@@ -50,6 +53,7 @@ class Board:
         board[self.__BoardSize - 1][self.__BoardSize - 1] = None
         self.__GoalBoard = board
         return board
+
     def Solve(self, H):
         self.__Forantier.append(H(self.getBoard()))
         self.__Forantier.sort()
@@ -59,31 +63,83 @@ class Board:
     def getSolution(self):
         return self.__Solution
 
-    def H1(self, board):
-        return None
+    def Hamming(self, board):
+        heuristic=0
+        for x in range(self.__BoardSize):
+            for y in range(self.__BoardSize):
+                if board[y][x] and self.__GoalBoard[y][x] != board[y][x]:
+                    heuristic += 1
+        return heuristic
 
     def Euclidean(self, board):
         heuristic = 0
-        for x1 in range(self.__GoalBoard):
-            for y1 in range(self.__GoalBoard[0]):
+        for x1 in range(self.__BoardSize):
+            for y1 in range(self.__BoardSize):
                 found = False
-                for x2 in range(board):
-                    for y2 in range(board[0]):
+
+                for x2 in range(self.__BoardSize):
+                    for y2 in range(self.__BoardSize):
                         if self.__GoalBoard[x1][y1] == board[x2][y2]:
                             found = True
-                            dx = int.__abs__(x2 - x1)
-                            dy = int.__abs__(y2 - y1)
-                            heuristic += int.__floor__(cmath.sqrt(dx*dx + dy*dy))
+                            dx = int(m.fabs(x2 - x1))
+                            dy = int(m.fabs(y2 - y1))
+                            heuristic += int(m.floor(m.sqrt(dx*dx + dy*dy)))
                             break
-                if(found):
-                    break
+                    if found:
+                        break
         return heuristic
 
-    def H3(self, board):
-        return None
+    def Manhattan(self, board):
+        heuristic = 0
+        board = np.array(board)
+        goal = np.array(self.__GoalBoard)
+        for x in range(self.__BoardSize):
+            for y in range(self.__BoardSize):
+                if board[y][x] and goal[y][x] != board[y][x]:
+                    row, col = np.where(goal == board[y][x])
+                    heuristic += abs(row[0] - x) + abs(col[0] - y)
+        return heuristic
 
-    def H4(self, board):
-        return None
+    def __count_conflicts(self,candidate_row, solved_row, ans=0):
+        counts = [0 for x in range(self.__BoardSize)]
+        for i, tile_1 in enumerate(candidate_row):
+            if tile_1 in solved_row and tile_1 != 0:
+                for j, tile_2 in enumerate(candidate_row):
+                    if tile_2 in solved_row and tile_2 != 0:
+                        if tile_1 != tile_2:
+                            if (solved_row.index(tile_1) > solved_row.index(tile_2)) and i < j:
+                                counts[i] += 1
+                            if (solved_row.index(tile_1) < solved_row.index(tile_2)) and i > j:
+                                counts[i] += 1
+        if max(counts) == 0:
+            return ans * 2
+        else:
+            i = counts.index(max(counts))
+            candidate_row[i] = -1
+            ans += 1
+            return self.__count_conflicts(candidate_row, solved_row, ans)
+
+
+    def linear_conflicts(self, board):
+        board = np.array(board).flatten()
+        goal = np.array(self.__GoalBoard).flatten()
+        res = self.manhattan(board)
+        candidate_rows = [[] for y in range(self.__BoardSize)]
+        candidate_columns = [[] for x in range(self.__BoardSize)]
+        solved_rows = [[] for y in range(self.__BoardSize)]
+        solved_columns = [[] for x in range(self.__BoardSize)]
+        for y in range(self.__BoardSize):
+            for x in range(self.__BoardSize):
+                idx = (y * self.__BoardSize) + x
+                candidate_rows[y].append(board[idx])
+                candidate_columns[x].append(board[idx])
+                solved_rows[y].append(goal[idx])
+                solved_columns[x].append(goal[idx])
+        for i in range(self.__BoardSize):
+            res += self.__count_conflicts(candidate_rows[i], solved_rows[i], self.__BoardSize)
+        for i in range(self.__BoardSize):
+            res += self.__count_conflicts(candidate_columns[i],solved_columns[i], self.__BoardSize)
+        return res
 
     def H5(self, board):
         return None
--- test.py
+++ test.py
@@ -1,26 +1,33 @@
 from creator import Creator
 from Actuators import Actuators
+import numpy as np
+from Board import Board
 
 #Create 8 puzzle
-# c = Creator(15)
-# x = c.initial_state()
-# g = c.goalState()
+c = Creator(8)
+x = c.initial_state()
 # print(x)
 # print(g)
+# A=Actuators()
+# res=A.move_up(arr)
+# print(res)
+# res2=A.move_up(res[1])
+# print(res2)
+# res3=A.move_down(res2[1])
+# print(res3)
+# res4=A.move_right(res3[1])
+# print(res4)
+# res5=A.move_left(res4[1])
+# print(res5)
 
-arr=[1,2,3,4,0,5,6,7,8]
-
-A=Actuators()
-
-res=A.move_up(arr)
-print(res)
-res2=A.move_up(res[1])
-print(res2)
-res3=A.move_down(res2[1])
-print(res3)
-res4=A.move_right(res3[1])
-print(res4)
-res5=A.move_left(res4[1])
-print(res5)
-
-
+a = Board(3)
+x = a.getBoard()
+c = [[1,None,8],[3,6,2],[7,4,1]]
+x = np.array(x)
+c = np.array(c)
+print(x)
+print(c)
+row , col = np.where(x == 6)
+print(row[0])
+print(col[0])
+print(a.Hamming(c))
