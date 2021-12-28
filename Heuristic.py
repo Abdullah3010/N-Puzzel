a8a160d51c0d6006395642053d943c5d6e501504
KamelAhmed813 <kamelahmed395@gmail.com>
2021-12-28 23:32:21 +0200

    Final Project ready for discussion missing the search space graph only

--- Heuristic.py
+++ Heuristic.py
@@ -39,49 +39,9 @@ class Heuristic:
             for y in range(len(board)):
                 if board[y][x] and goal[y][x] != board[y][x]:
                     row, col = np.where(goal == board[y][x])
-                    heuristic += abs(row[0] - x) + abs(col[0] - y)
+                    heuristic += abs(row[0] - y) + abs(col[0] - x)
         return heuristic
-    '''
-    def __count_conflicts(self, candidate_row, solved_row, ans=0):
-        counts = [0 for x in range(len(candidate_row))]
-        for i, tile_1 in enumerate(candidate_row):
-            if tile_1 in solved_row and tile_1 != 0:
-                for j, tile_2 in enumerate(candidate_row):
-                    if tile_2 in solved_row and tile_2 != 0:
-                        if tile_1 != tile_2:
-                            if (solved_row.index(tile_1) > solved_row.index(tile_2)) and i < j:
-                                counts[i] += 1
-                            if (solved_row.index(tile_1) < solved_row.index(tile_2)) and i > j:
-                                counts[i] += 1
-        if max(counts) == 0:
-            return ans * 2
-        else:
-            i = counts.index(max(counts))
-            candidate_row[i] = -1
-            ans += 1
-            return self.__count_conflicts(candidate_row, solved_row, ans)
 
-    def linear_conflicts(self, board):
-        res = self.Manhattan(board)
-        board = np.array(board).flatten()
-        goal = np.array(Board(len(board)).getBoard()).flatten()
-        candidate_rows = [[] for y in range(len(board))]
-        candidate_columns = [[] for x in range(len(board))]
-        solved_rows = [[] for y in range(len(board))]
-        solved_columns = [[] for x in range(len(board))]
-        for y in range(len(board)):
-            for x in range(len(board)):
-                idx = (y * len(board)) + x
-                candidate_rows[y].append(board[idx])
-                candidate_columns[x].append(board[idx])
-                solved_rows[y].append(goal[idx])
-                solved_columns[x].append(goal[idx])
-        for i in range(len(board)):
-            res += self.__count_conflicts(candidate_rows[i], solved_rows[i], len(board))
-        for i in range(len(board)):
-            res += self.__count_conflicts(candidate_columns[i], solved_columns[i], len(board))
-        return res
-'''
     def Permutation(self, board):
         heuristic = 0
         board = np.array(board)
